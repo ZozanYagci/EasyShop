a279cd4645a0041cd7586d563d53c3dc92d646a2
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-03-17 22:52:35 +0300

    added Areas for user and admin

---
+++ EasyShop.UI/Areas/User/ViewComponents/UserLayoutViewComponents/_UserLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyShop.UI.Areas.User.ViewComponents.UserLayoutViewComponents
+{
+    public class _UserLayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
